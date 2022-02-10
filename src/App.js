b53083fcc0ee5bc0e77d32f21bd939a8096e6453
Almir Sulejmanovic <16432927+almirsulejmanovic@users.noreply.github.com>
2022-02-10 15:15:39 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  const [number, setNumber] = useState(0)
+
+  function getNumber() {
+    const interval = setInterval(() => { setNumber(Math.floor(Math.random() * 99999 + 1)) }, 100);
+    setTimeout(() => { clearInterval(interval); displayAlert() }, 3000)
+  }
+
+  function displayAlert() {
+    if (number >= 1 && number <= 1000) {
+      alert("Congratulations! You've won!");
+    } else {
+      alert("Too bad! You've lost!");
+    }
+  }
+
+
+  return (
+    <div className='app'>
+      <div className='title'>
+        <h1>The JavaScript Lottery</h1>
+      </div>
+
+      <div className='btn'>
+        <h2>Click the button to draw a number.</h2>
+        <button onClick={() => {
+          getNumber();
+        }}>
+          Let's Play!
+        </button>
+        <h3>Your Number: {number}</h3>
+      </div>
+
+      <h2>* Any number in the 1 to 1000 range wins!</h2>
+    </div>
+  )
+}
